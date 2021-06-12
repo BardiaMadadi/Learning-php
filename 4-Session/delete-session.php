db2d4c219619499b62de1f0d57868f8473e1b690
Bardia Madadi <bardiamadadi86@gmail.com>
2021-06-12 16:32:31 +0430

    Do session project

---
+++ 4-Session/delete-session.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+unset($_SESSION['name']);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>show session</title>
+</head>
+
+<body>
+<a href="./show-session.php"> SHOW </a>
+</body>
+
+</html>
---
+++ 4-Session/show-session.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if(isset($_SESSION['name'])){
+echo $_SESSION['name'];
+}else{
+    echo 'WE DO NOT HAVE THAT  SESSION';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>show session</title>
+</head>
+
+<body>
+<a href="./delete-session.php"> DELETE </a>
+</body>
+
+</html>
